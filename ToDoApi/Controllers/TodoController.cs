daa92a076dcf85fcbe2337ec9eaa2da80aa00ec6
Kashmitha <kashmithamadushan@gmail.com>
2025-06-07 12:25:17 +0530

    2nd Edit: Created Controller

---
+++ ToDoApi/Controllers/TodoController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Data;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/todo
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        // GET: api/todo/{id}
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> Get(int id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        // POST: api/todo
+        [HttpPost]
+        public ActionResult<TodoItem> Create(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new { id = item.Id }, item);
+        }
+
+        // PUT: api/todo/{id}
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, TodoItem item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+
+            _context.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/todo/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(item);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
